34cd1e3f87b79619d182fc4b2d87efb9790af6a2
saadii077 <saadkashmiri783@gmail.com>
2024-09-21 18:20:24 +0500

    Done!!

---
+++ app/components/About.tsx
@@ -0,0 +1,8 @@
+function About() {
+  return (
+    <div style={{ marginLeft: "30px", color: "green" }}>
+      <h1>//This is About page</h1>
+    </div>
+  );
+}
+export default About;
---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <div style={{ marginLeft: "100px", padding: "20px", color: "Highlight" }}>
+      <h1> //This is Header page</h1>
+    </div>
+  );
+}
+export default Header;
